c2e660aa3f39690dd9f14c143b2889162fb818c3
alexb <alexbarladeanu@gmail.com>
2021-05-20 15:19:16 +0300

    Homework1

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-name = ["My", "name", "is", "Alex", "Birladeanu"]
-i1, i2, i3, *i4 = name
-for i in i4:
+my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+
+asc = sorted(my_list)
+print("Ascending order:")
+for i in asc:
     print(i)
+
+desc = sorted(my_list, reverse=True)
+print("Descending order:")
+for i in desc:
+    print(i)
+
+odd_numbers = asc[::2]
+print("Odd numbers:")
+for i in odd_numbers:
+    print(i)
+
+even_numbers = asc[1::2]
+print("Even numbers:")
+for i in even_numbers:
+    print(i)
+
+print("Multiples of 3:")
+for i in my_list:
+    if i % 3 == 0:
+        print(i)
